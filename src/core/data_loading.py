15a95427a1995fa7c1405ea68a004fc9bb5e32a7
Laura Shehaj <Shehulaura13@gmail.com>
2026-05-25 09:53:38 +0200

    Update gitignore and clean project artifacts

--- src/core/data_loading.py
+++
@@ -1,9 +0,0 @@
-
-import pandas as pd
-
-def data_loading(path):
-    df=pd.read_csv(path)
-    return df
-
-
-
--- src/utils/utils.py
+++
@@ -1,8 +0,0 @@
-import joblib
-import os
-
-
-
-def save_model(model_obj, path):
-    os.makedirs(os.path.dirname(path), exist_ok=True)
-    joblib.dump(model_obj, path)
